f9bed2dc5471ec216fe016697e854bec6b0d387d
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-01-20 23:39:05 +0530

    intial commit

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Header from "./header/Header1.jsx";
 import Footer from "./footer/Footer1.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
-import LogoutBtn from "./Header/LogoutBtn.jsx";
+import LogoutBtn from "./header/LogoutBtn.jsx";
 import RTE from "./RTE.jsx";
 import Signup from "./Signup.jsx";
 import Login from "./Login.jsx";
